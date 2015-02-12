b312908142cb9bdf205f70de9907ed32466e0267
Vladimir Grinenko <i@tadatuta.com>
2015-02-12 16:06:55 +0300

    fix deps

--- common.blocks/test-block/test-block.deps.js
+++ common.blocks/test-block/test-block.deps.js
@@ -1,6 +1,11 @@
-({
-    tech : 'spec.js',
-    mustDeps : [
-        { tech : 'js', block : 'i-bem', elem : 'dom' }
-    ]
-})
+[
+    {
+        mustDeps : ['i-bem']
+    },
+    {
+        tech : 'spec.js',
+        mustDeps : [
+            { tech : 'js', block : 'i-bem', elem : 'dom' }
+        ]
+    }
+]
